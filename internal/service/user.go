8bb1ca61f770987f1f7439e8b42866df39ba2ace
VinceZCL <vincewcode@gmail.com>
2025-11-14 17:53:17 +0800

    chore: error on app/app.go

---
+++ internal/service/user.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"github.com/labstack/echo/v4"
+	"scrum.com/internal/repository"
+	"scrum.com/types/models/dto"
+)
+
+type UserService struct {
+	repo repository.UserRepository
+}
+
+func NewUserService(repo repository.UserRepository) *UserService {
+	return &UserService{repo: repo}
+}
+
+func (s *UserService) GetMembers(c echo.Context, teamID int) ([]dto.Member, error) {
+
+	users, err := s.repo.GetMembers(teamID)
+	if err != nil {
+		c.Logger().Errorf("Service | UserService | GetMembers: %v", err)
+		return nil, err
+	}
+
+	dtos := make([]dto.Member, len(users))
+	for i, u := range users {
+		dtos[i] = dto.Member{
+			Name:     u.User.Name,
+			Email:    u.User.Email,
+			TeamName: u.Team.Name,
+			Role:     u.Role,
+		}
+	}
+	return dtos, nil
+}
