c7c926cc2eabf63d06bf92de7ad9b962b80a082b
VinceZCL <vincewcode@gmail.com>
2025-12-23 11:11:11 +0800

    feat: auth middleware for bearer

--- internal/endpoint/rest.go
+++ internal/endpoint/rest.go
@@ -1,29 +1,41 @@
 package endpoint
 
 import (
+	"github.com/VinceZCL/FinalYearProject/internal/endpoint/middlewares"
 	"github.com/VinceZCL/FinalYearProject/internal/handler"
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterRoutes(e echo.Echo) {
 	e.GET("/", handler.Hello)
-	e.GET("/members/teams/:id", handler.GetMembers)
-	e.GET("/users", handler.GetUsers)
-	e.GET("/teams", handler.GetTeams)
-
-	e.GET("/checkins/users/:id", handler.GetUserCheckIns)
-	e.GET("/checkins/teams/:id", handler.GetTeamCheckIns)
-
-	e.GET("/users/:id", handler.GetUser)
-	e.GET("/teams/:id", handler.GetTeam)
-	e.GET("/checkins/:id", handler.GetCheckIn)
-
-	e.GET("/teams/users/:id", handler.GetUserTeams)
-
-	e.POST("/teams", handler.NewTeam)
-	e.POST("/members", handler.NewMember)
-	e.POST("/checkins", handler.NewCheckIn)
-
 	e.POST("/auth/register", handler.Register)
 	e.POST("/auth/login", handler.Login)
+	e.POST("/auth/logout", handler.Logout)
+
+	protected := e.Group("")
+	protected.Use(middlewares.AuthMiddleware())
+
+	// GET ALL
+	protected.GET("/users", handler.GetUsers)
+	protected.GET("/teams", handler.GetTeams)
+
+	// GET SINGLE
+	protected.GET("/users/:id", handler.GetUser)
+	protected.GET("/teams/:id", handler.GetTeam)
+	protected.GET("/checkins/:id", handler.GetCheckIn)
+
+	// GET TEAM RELATED
+	protected.GET("/members/teams/:id", handler.GetMembers)
+	protected.GET("/teams/users/:id", handler.GetUserTeams)
+
+	// GET CHECKINS
+	protected.GET("/checkins/users/:id", handler.GetUserCheckIns)
+	protected.GET("/checkins/teams/:id", handler.GetTeamCheckIns)
+
+	// CREATE
+	protected.POST("/teams", handler.NewTeam)
+	protected.POST("/members", handler.NewMember)
+	protected.POST("/checkins", handler.NewCheckIn)
+
+	// TODO PUT METHODS
 }
