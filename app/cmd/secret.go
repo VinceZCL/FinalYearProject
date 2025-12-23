0dd7c6be1632c12add15d9d772d7f9f91326a7da
VinceZCL <vincewcode@gmail.com>
2025-12-23 11:24:58 +0800

    chore: migrate secret generator to golang

---
+++ app/cmd/secret.go
@@ -0,0 +1,34 @@
+//go:build cli
+// +build cli
+
+package cmd
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "secret",
+		Short: "Generate secret",
+		Run: func(cmd *cobra.Command, args []string) {
+			key := generateJWTSecretKey()
+			fmt.Println("Add this into `.env.local`")
+			fmt.Printf("export SECURITY_SECRETKEY=%s\n", key)
+		},
+	})
+}
+
+func generateJWTSecretKey() string {
+	secret := make([]byte, 32)
+	_, err := rand.Read(secret)
+	if err != nil {
+		log.Fatalf("Error generating secret: %w", err)
+	}
+	return base64.RawURLEncoding.EncodeToString(secret)
+}
