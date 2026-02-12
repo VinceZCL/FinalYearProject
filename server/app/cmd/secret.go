ceefa1bfaf8a73dbbc2fb82b3555e015c79d92a5
VinceZCL <vincewcode@gmail.com>
2026-02-12 10:24:30 +0800

    feat: auto secret env

--- server/app/cmd/secret.go
+++ server/app/cmd/secret.go
@@ -8,6 +8,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -18,8 +19,25 @@ func init() {
 		Short: "Generate secret",
 		Run: func(cmd *cobra.Command, args []string) {
 			key := generateJWTSecretKey()
-			fmt.Println("Add this into `./server/.env.local`")
-			fmt.Printf("export SECURITY_SECRETKEY=%s\n", key)
+
+			// Path to .env.local, relative to where the command is run (i.e., 'server')
+			envPath := ".env.local"
+
+			// Open file for writing (will create the file if it doesn't exist)
+			f, err := os.OpenFile(envPath, os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Fatalf("Failed to open %s: %v", envPath, err)
+			}
+			defer f.Close()
+
+			// Write the SECURITY_SECRETKEY line into the file
+			line := fmt.Sprintf("export SECURITY_SECRETKEY=%s\n", key)
+			if _, err := f.WriteString(line); err != nil {
+				log.Fatalf("Failed to write secret: %v", err)
+			}
+
+			fmt.Printf("Secret written to server/%s\n", envPath)
+
 		},
 	})
 }
